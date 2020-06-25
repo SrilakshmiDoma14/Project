46696073b5c87e04800c22b64d0420905d5a2cc3
Srilakshmi Doma <60988616+SrilakshmiDoma14@users.noreply.github.com>
2020-06-25 13:34:01 -0400

    commit4

--- exercise.py
+++ exercise.py
@@ -2,14 +2,15 @@ import datetime
 import subprocess
 import time
 
-cmd = ['./exercise.exe']
+cmd = ['/root/sravya/exercise.exe']
 while True:
     result = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     datetime_object = datetime.datetime.now()
 
-    with open('index.html', "w") as f:
+    with open('/usr/share/nginx/html/index.html', "w") as f:
         f.write("<html><body>Hi from python, time: <b>{0}</b></p><br />".format(datetime_object))
         f.write("<p>from C: <b>{0}</b></p></body></html><br />".format(result))
 
     time.sleep(5)
+

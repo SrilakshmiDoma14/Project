db0a3e743850dec091dd1eed9c6cab98b25a2a6d
Srilakshmi Doma <60988616+SrilakshmiDoma14@users.noreply.github.com>
2020-06-25 14:36:12 -0400

    5

--- exercise.py
+++ exercise.py
@@ -9,8 +9,8 @@ while True:
     datetime_object = datetime.datetime.now()
 
     with open('/usr/share/nginx/html/index.html', "w") as f:
-        f.write("<html><body>Hi from python, time: <b>{0}</b></p><br />".format(datetime_object))
-        f.write("<p>from C: <b>{0}</b></p></body></html><br />".format(result))
+        f.write("<html><body>Hi from python, time: <b>{}</b></p><br />".format(datetime_object))
+        f.write("<p>from C: <b>{}</b></p></body></html><br />".format(result))
 
     time.sleep(5)
 

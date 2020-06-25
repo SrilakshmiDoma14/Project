bc1b422eb181dcdaae56b4f44b5ff8d4a6d28350
Srilakshmi Doma <60988616+SrilakshmiDoma14@users.noreply.github.com>
2020-06-25 19:43:37 -0400

    commit made to test

--- exercise.py
+++ exercise.py
@@ -9,7 +9,7 @@ while True:
     datetime_object = datetime.datetime.now()
 
     with open('/var/www/html/index.html', "w") as f:
-        f.write("<html><body>Hi from python, time: <b>{0}</b></p><br />".format(datetime_object))
+        f.write("<html><body>Hello from python, time: <b>{0}</b></p><br />".format(datetime_object))
         f.write("<p>from C: <b>{0}</b></p></body></html><br />".format(result))
 
     time.sleep(5)
